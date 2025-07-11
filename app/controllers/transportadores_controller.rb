b1e84ba5b8bb50ff3deab90902daf96f2a9b28a0
Harley Soncim <harleysoncim@gmail.com>
2025-07-11 18:30:56 -0300

    Ajuste final: controllers REST prontos para todas as views

---
+++ app/controllers/transportadores_controller.rb
@@ -0,0 +1,2 @@
+class TransportadoresController < ApplicationController
+end
---
+++ test/controllers/fretes_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class FretesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
