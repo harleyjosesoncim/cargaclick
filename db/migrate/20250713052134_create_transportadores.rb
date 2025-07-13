5e16da6a48c7b351b2333b0e78b287bd0485504b
Harley Soncim <harleysoncim@gmail.com>
2025-07-13 03:29:17 -0300

    Adiciona campos observacoes (50 chars) e alba_numero ao cliente

--- db/migrate/20250713052134_create_transportadores.rb
+++ db/migrate/20250713052134_create_transportadores.rb
@@ -2,11 +2,17 @@ class CreateTransportadores < ActiveRecord::Migration[7.1]
   def change
     create_table :transportadores do |t|
       t.string :nome
+      t.string :cpf
       t.string :telefone
-      t.string :cnpj
-      t.decimal :volume
-      t.decimal :peso
-
+      t.string :endereco
+      t.string :cep
+      t.string :tipo_veiculo
+      t.string :carga_maxima
+      t.decimal :valor_km
+      t.decimal :largura
+      t.decimal :altura
+      t.decimal :profundidade
+      t.decimal :peso_aproximado
       t.timestamps
     end
   end
