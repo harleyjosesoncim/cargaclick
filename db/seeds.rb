155f32c1d626bc0c4b842b19e424829c87bf7297
Harley Soncim <harleysoncim@gmail.com>
2025-08-11 20:48:26 -0300

    chore(seeds): clientes de exemplo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Clientes de exemplo (Devise)
+clientes = [
+  { email: "ana@exemplo.com",  password: "senha123" },
+  { email: "bruno@exemplo.com", password: "senha123" },
+  { email: "carla@exemplo.com", password: "senha123" }
+]
+
+clientes.each do |attrs|
+  Cliente.find_or_create_by!(email: attrs[:email]) do |c|
+    c.password = attrs[:password]
+    c.password_confirmation = attrs[:password]
+  end
+end
+puts "[seeds] Clientes criados/garantidos: #{clientes.map { _1[:email] }.join(', ')}"
