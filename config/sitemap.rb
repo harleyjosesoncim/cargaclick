838e01b56caa8cdeb13c2ae3dc6fac14e85eb8bb
Harley Soncim <harleysoncim@gmail.com>
2025-08-12 21:26:49 -0300

    chore(seo): sitemap.rb robusto + sitemap.xml(.gz) gerado

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,4 +1,7 @@
-cat > config/sitemap.rb <<'RUBY'
+# config/sitemap.rb
+# Configuração do Sitemap do CargaClick (Rails)
+# Gera public/sitemap.xml(.gz) para o Google e outros crawlers.
+
 SitemapGenerator::Sitemap.default_host = "https://www.cargaclick.com.br"
 SitemapGenerator::Sitemap.public_path  = "public/"
 SitemapGenerator::Sitemap.compress     = true
@@ -8,8 +11,6 @@ SitemapGenerator::Sitemap.create do
   add '/fretes/novo',           changefreq: 'daily',   priority: 0.9
   add '/clientes/novo',         changefreq: 'monthly', priority: 0.6
   add '/transportadores/novo',  changefreq: 'monthly', priority: 0.6
-  # add outras rotas públicas aqui, ex:
+  # add outras rotas públicas aqui, ex.:
   # add '/ranking',             changefreq: 'weekly'
 end
-RUBY
-# This file is used to configure the sitemap generation for the CargoClick application.
