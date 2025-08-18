2143d1ed0283d0711c80fd33cc96ca492675d51c
Harley Soncim <harleysoncim@gmail.com>
2025-08-17 21:29:54 -0300

    fix: melhora fluxo de cadastro de clientes

--- app/controllers/clientes/registrations_controller.rb
+++ app/controllers/clientes/registrations_controller.rb
@@ -1,17 +1,29 @@
 # frozen_string_literal: true
 
 class Clientes::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   protected
 
   # Redireciona após cadastro concluído
   def after_sign_up_path_for(resource)
-    sucesso_cadastro_path  # <-- Caminho da tela de sucesso
+    sucesso_cadastro_path || root_path
   end
 
-  # Caso queira permitir campos extras no cadastro (ex: nome)
+  # Redireciona após atualização de perfil
+  def after_update_path_for(resource)
+    cliente_dashboard_path || root_path
+  end
+
+  # Permitir campos extras no cadastro
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nome])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nome, :telefone, :cpf])
   end
 
+  # Permitir campos extras na atualização
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nome, :telefone, :cpf])
+  end
 end
+
---
+++ app/controllers/clientes/sessions_controller.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+class Clientes::SessionsController < Devise::SessionsController
+  layout "devise"
+end
