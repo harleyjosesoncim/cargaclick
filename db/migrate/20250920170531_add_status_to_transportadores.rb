e68e4b298d7269d9632ef503048fa2c76ef454e8
Harley Jose Soncim <harleysoncim@gmail.com>
2025-09-20 14:07:52 -0300

    AddStatusToTransportadores migration aplicada

---
+++ db/migrate/20250920170531_add_status_to_transportadores.rb
@@ -0,0 +1,6 @@
+class AddStatusToTransportadores < ActiveRecord::Migration[7.1]
+  def change
+    add_column :transportadores, :status, :integer, default: 0, null: false
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_18_200155) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_20_170531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -259,6 +259,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_18_200155) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.string "chave_pix"
+    t.integer "status", default: 0, null: false
     t.index ["confirmation_token"], name: "index_transportadores_on_confirmation_token", unique: true
     t.index ["email"], name: "index_transportadores_on_email", unique: true, where: "(email IS NOT NULL)"
     t.index ["reset_password_token"], name: "index_transportadores_on_reset_password_token", unique: true
