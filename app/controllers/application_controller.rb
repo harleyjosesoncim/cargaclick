225389f8f372b2b8bd2814c68808651561b7ce20
Harley Jose Soncim <harleysoncim@gmail.com>
2026-01-10 06:31:03 -0300

    refactor: remove global authentication logic and stabilize access control

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,20 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :warning, :info
 
-  before_action :authenticate_scope!, unless: :public_page?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # =========================================================
-  # ROTAS PÚBLICAS (NÃO EXIGEM LOGIN)
-  # =========================================================
-  PUBLIC_ROUTES = [
-    { controller: "landing",      actions: %w[index] },
-    { controller: "home",         actions: %w[index about fidelidade] },
-    { controller: "fretes",       actions: %w[new create] }, # simulação pública
-    { controller: "contatos",     actions: %w[new create] },
-    { controller: "pagamentos",   actions: %w[webhook ping sucesso falha pendente retorno] },
-    { controller: "rails/health", actions: %w[show] }
-  ].freeze
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[nome telefone cpf])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[nome telefone cpf])
+  end
 
   def after_sign_in_path_for(resource)
     case resource
@@ -30,37 +24,5 @@ class ApplicationController < ActionController::Base
     else
       super
     end
-  end
-
-  protected
-
-  def authenticate_scope!
-    return if devise_controller?
-
-    if respond_to?(:admin_signed_in?, true) && admin_signed_in?
-      return authenticate_admin! if respond_to?(:authenticate_admin!, true)
-    end
-
-    if respond_to?(:transportador_signed_in?, true) && transportador_signed_in?
-      return authenticate_transportador! if respond_to?(:authenticate_transportador!, true)
-    end
-
-    if respond_to?(:cliente_signed_in?, true) && cliente_signed_in?
-      return authenticate_cliente! if respond_to?(:authenticate_cliente!, true)
-    end
-
-    true
-  end
-
-  def public_page?
-    PUBLIC_ROUTES.any? do |route|
-      route[:controller] == controller_path &&
-        route[:actions].include?(action_name)
-    end
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[nome telefone cpf role])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[nome telefone cpf role])
   end
 end
