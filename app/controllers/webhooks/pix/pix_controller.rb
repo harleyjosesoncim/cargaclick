d99f1e5a935fad0c644a92cac0cf11d23c23e1fa
Harley Jose Soncim <harleysoncim@gmail.com>
2026-01-12 17:01:55 -0300

    fix: ajusta PixController e atualiza views do dashboard e fidelidade

--- app/controllers/webhooks/pix/pix_controller.rb
+++ app/controllers/webhooks/pix/pix_controller.rb
@@ -1,22 +1,44 @@
-def processar_evento(data)
-  pix = data["pix"]&.first
-  return unless pix
+module Webhooks
+  module Pix
+    class PixController < ApplicationController
+      skip_before_action :verify_authenticity_token
 
-  txid = pix["txid"]
-  return if txid.blank?
+      def create
+        begin
+          payload = params.to_unsafe_h
+          processar_eventos(payload["pix"]) if payload["pix"].is_a?(Array)
+          head :ok
+        rescue => e
+          Rails.logger.error("[WEBHOOK PIX EFI] Erro ao processar webhook: #{e.message}")
+          Rails.logger.error(e.backtrace.join("\n"))
+          head :internal_server_error
+        end
+      end
 
-  frete = Frete.find_by(pix_txid: txid)
+      private
 
-  unless frete
-    Rails.logger.warn("[WEBHOOK PIX EFI] Frete não encontrado para txid=#{txid}")
-    return
+      def processar_eventos(pix_array)
+        pix_array.each do |pix|
+          txid = pix["txid"]
+          next if txid.blank?
+
+          frete = Frete.find_by(pix_txid: txid)
+
+          unless frete
+            Rails.logger.warn("[WEBHOOK PIX EFI] Frete não encontrado para txid=#{txid}")
+            next
+          end
+
+          if frete.status_pagamento == "pago"
+            Rails.logger.info("[WEBHOOK PIX EFI] Frete ##{frete.id} já está marcado como pago.")
+            next
+          end
+
+          frete.update!(status_pagamento: "pago")
+          Rails.logger.info("[WEBHOOK PIX EFI] Frete ##{frete.id} marcado como pago com sucesso.")
+        end
+      end
+    end
   end
-
-  return if frete.status_pagamento == "pago"
-
-  Rails.logger.info("[WEBHOOK PIX EFI] Frete ##{frete.id} encontrado. Marcando como pago.")
-
-  frete.update!(
-    status_pagamento: "pago"
-  )
 end
+
